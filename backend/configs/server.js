5697afc29769b20553b3c804084502c5c70f6108
Jonathan <jdomingo-2022206@kinal.edu.gt>
2024-04-19 14:04:22 -0600

    Server, ConectDB, Auth already

--- backend/configs/server.js
+++ backend/configs/server.js
@@ -4,19 +4,29 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { dbConnection } from './mongo.js';
+import authRoutes from '../src/auth/auth.routes.js'
 // import routes from 'routes.js';
 
 class Server {
     constructor() {
+        this.notes();
         this.app = express();
         this.port = process.env.PORT;
         this.converPath = '/lodgy/v1/';
-        
+        this.authPath = '/lodgy/v1/auth'
+
         this.middlewares();
+        this.conectDB();
         this.routes();
     }
 
+    async conectDB(){
+        await dbConnection()
+    }
+
     routes() {
+        this.app.use(this.authPath , authRoutes);
         // this.app.use(this.converPath, routes);
     }
 
@@ -32,6 +42,16 @@ class Server {
         this.app.listen(this.port, () => {
             console.log('Server running on port ', this.port);
         });
+    }
+
+    notes() {
+        console.log('');
+        console.log('');
+        console.log('NOTE: Server constructor called!');
+        console.log('if port 3000 is in use:');
+        console.log('netstat -ano | findstr :3000');
+        console.log('taskkill /PID <PID> /F');
+        console.log('');
     }
 }
 
