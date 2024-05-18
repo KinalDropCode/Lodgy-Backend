b1baf31062399ab0cf1df64c02938cf49ca93651
mvalladares-2019607 <mvalladares-2019607@kinal.edu.gt>
2024-05-17 22:19:23 -0600

    add routes

--- src/reservation/reservation.routes.js
+++ src/reservation/reservation.routes.js
@@ -4,6 +4,9 @@ import { validateJWT } from '../middlewares/validate-jwt.js';
 
 const api = express.Router(); 
 api.post('/create', validateJWT, reservationPost)
+api.delete('/delete/:id', validateJWT, deleteReservationId)
+api.get('/:id', validateJWT, getReservationById)
+api.put('/update/:id', validateJWT, reservationUpdate)
 
 
 export default api
